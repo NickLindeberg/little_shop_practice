d2e290465a2ee5ebf659f046271917c4b48a5cbc
Nick Lindeberg <nlindeberg@vynyl.com>
2019-11-10 00:54:45 -0700

    update item factory

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -2,8 +2,8 @@ FactoryBot.define do
   factory :item do
     name { "Cool Item" }
     description { "Really cool thing" }
-    price { 10.00 }
+    price { 10 }
     image { "MyString" }
-    inventory { "MyString" }
+    inventory { rand 1..10  }
   end
 end
