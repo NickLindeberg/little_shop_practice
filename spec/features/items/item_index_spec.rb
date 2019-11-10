07cc5ff95bea06f7351dc36f5ab83cd4879c17c7
Nick Lindeberg <nlindeberg@vynyl.com>
2019-11-10 00:55:09 -0700

    item index with test

--- spec/features/items/item_index_spec.rb
+++ spec/features/items/item_index_spec.rb
@@ -1,12 +1,23 @@
 require 'rails_helper'
 
 describe 'Item Index' do
-
   context 'when a visitor visits /items' do
-
     it 'shows an index of items' do
+      item_1 = FactoryBot.create(:item)
+      item_2 = FactoryBot.create(:item)
 
+      visit items_path
 
+      expect(page).to have_content(item_1.name)
+      expect(page).to have_content(item_1.description)
+      expect(page).to have_content(item_1.price)
+      expect(page).to have_content(item_1.image)
+      expect(page).to have_content(item_1.inventory)
+      expect(page).to have_content(item_2.name)
+      expect(page).to have_content(item_2.description)
+      expect(page).to have_content(item_2.price)
+      expect(page).to have_content(item_2.image)
+      expect(page).to have_content(item_2.inventory)
     end
   end
 end
